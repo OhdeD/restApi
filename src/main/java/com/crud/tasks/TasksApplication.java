549ba0cff6586fa6e49596c612e1ddce8ec053f4
OhdeD <dagmara.ohde@gmail.com>
2019-09-21 15:26:27 +0000

    Moduł18, zadanie 4

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -8,9 +8,8 @@ import java.sql.SQLOutput;
 
 @SpringBootApplication
 public class TasksApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
-	}
 
+	}
 }
