c96564710ce19f63ca5d9ed0a4223c359b5931ed
OhdeD <dagmara.ohde@gmail.com>
2019-12-30 20:36:21 +0100

    Moduł 32, zadanie 2

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.config.AdminConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -8,6 +9,10 @@ import org.thymeleaf.context.Context;
 
 @Service
 public class MailCreatorService {
+
+    @Autowired
+    private AdminConfig adminConfig;
+
     @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
@@ -17,6 +22,10 @@ public class MailCreatorService {
         context.setVariable("message", message);
         context.setVariable("task_url", "https://ohded.github.io/");
         context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("preview_message", message.substring(0,50));
+        context.setVariable("goodbye", "Best regards, " + adminConfig.getAdminName() + ".");
+        context.setVariable("company", adminConfig.getCompanyName() + " || " + adminConfig.getCompanyStreet() + " " + adminConfig.getCompanyNo() + " || e-mail: " + adminConfig.getCompanyMail() + " || phone: " + adminConfig.getCompanyNumber());
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 
